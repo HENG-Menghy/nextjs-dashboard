e68f727c8f081dabc7f2ddfa3c07c7c41014a9b2
HENG-Menghy <hengmenghy2002@gmail.com>
2025-04-06 04:57:26 +0700

    Alias folder path name, create dashboard layout and navigation, and fonts.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import "@ui/global.css";
+import { Metadata } from "next";
+import { inter } from "@ui/fonts";
+
+export const metadeta: Metadata = {
+  title: "Next.js dashboard Full-Stack App",
+  description: "Build dynamic, and modern dashboard app using Next.js with PostgreSQL"
+}
 
 export default function RootLayout({
   children,
@@ -7,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body> {children} </body>
+      <body className={`${inter.className}`}> {children} </body>
     </html>
   );
 }
