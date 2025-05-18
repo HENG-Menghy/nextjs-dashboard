391e2ace0fc14c2482591c7e83029694224103a6
HENG-Menghy <hengmenghy2002@gmail.com>
2025-05-18 12:09:45 +0700

    copleted mutating data in chapter 12

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable PPR (Partial Prerendering) by adding the 'ppr' option 
+  experimental: {
+    ppr: 'incremental'
+  }
+
 };
 
 export default nextConfig;
